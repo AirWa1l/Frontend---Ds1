2b695efea7f02b042cbc9f15ea34f175206e4cd5
michaelRS2002 <148493262+michaelRS2002@users.noreply.github.com>
2024-12-13 01:43:21 -0500

    Update OrderSummary.jsx

--- src/components/Cart/OrderSummary/OrderSummary.jsx
+++ src/components/Cart/OrderSummary/OrderSummary.jsx
@@ -5,7 +5,10 @@ import { toast } from "react-toastify";
 
 const OrderSummary = () => {
   const { store, modal, auth } = useGlobalContext();
-  const [deliveryType, setDeliveryType] = useState("Standard");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [state, setState] = useState("");
   const [phone, setPhone] = useState("");
   const setDelivery = (type) => {
     setDeliveryType(type);
@@ -30,73 +33,127 @@ const OrderSummary = () => {
     <div className="is-order-summary">
       <div className="sub-container">
         <div className="contains-order">
+          {/* Resumen de los costos */}
           <div className="total-cost">
-          <h4>Total Items({store.state.cartQuantity})</h4><br />
+            <h4>Total Items({store.state.cartQuantity})</h4>
+            <br />
             <h3>SUBTOTAL </h3>
-            <h3>$&nbsp;
-    {new Intl.NumberFormat("es-ES", {
-      style: "decimal",
-      useGrouping: true,
-    }).format(Math.floor(store.state.cartTotal|| 0))} {/* Corrección aplicada */}
-  </h3>
+            <h3>
+              $&nbsp;
+              {new Intl.NumberFormat("es-ES", {
+                style: "decimal",
+                useGrouping: true,
+              }).format(Math.floor(store.state.cartTotal || 0))}
+            </h3>
           </div>
+
           <div className="shipping">
-            <h4>Shipping  ({store.state.cartQuantity})</h4>
-            <select
-              className="select-dropdown"
-              onChange={(item) => {
-                setDelivery(item.target.value);
-              }}
+            <h4>Shipping ({store.state.cartQuantity})</h4>
+          </div>
+
+          <div className="enter-promo">
+            <input className="select-dropdown" type="text" />
+            <button
+              className="flat-button apply-promo"
+              disabled={store.state.cartQuantity > 0 ? false : true}
             >
-              
-            </select>
+              Apply
+            </button>
           </div>
+
+          {/* Campos de información de contacto */}
           <div className="promo-code">
-            <h4>Promo Code</h4>
-            <div className="enter-promo">
-              <input className="select-dropdown" type="text" />
-              <button
-                className="flat-button apply-promo"
-                disabled={store.state.cartQuantity > 0 ? false : true}
-              >
-                Apply
-              </button>
+            <h4>Información de contacto</h4>
+            <div>
+              <label htmlFor="address">Dirección</label>
+              <input
+                id="address"
+                className="select-dropdown"
+                type="text"
+                placeholder="Ingrese su dirección"
+                onChange={(e) => setAddress(e.target.value)}
+              />
             </div>
-          </div>
-          <div className="promo-code">
-            <h4>Phone Number</h4>
-            <input
-              className="select-dropdown"
-              type="text"
-              onChange={(item) => {
-                setPhone(item.target.value);
-              }}
-            />
+
+            <div>
+              <label htmlFor="city">Ciudad</label>
+              <input
+                id="city"
+                className="select-dropdown"
+                type="text"
+                placeholder="Ingrese su ciudad"
+                onChange={(e) => setCity(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="country">País</label>
+              <input
+                id="country"
+                className="select-dropdown"
+                type="text"
+                placeholder="Ingrese su país"
+                onChange={(e) => setCountry(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="state">Estado</label>
+              <input
+                id="state"
+                className="select-dropdown"
+                type="text"
+                placeholder="Ingrese su estado"
+                onChange={(e) => setState(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="phone">Teléfono</label>
+              <input
+                id="phone"
+                className="select-dropdown"
+                type="text"
+                placeholder="Ingrese su número de teléfono"
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+
             <small>
               <em style={{ color: "#ff2100" }}>
-                Your number would be called to verify the order placement
+                Su número será utilizado para verificar la colocación del pedido
               </em>
             </small>
           </div>
+
+          {/* Resumen del costo total */}
           <div className="final-cost">
             <h4>Total Cost</h4>
             <h4>
-              ${" "}
+              $
               {store.state.cart.length > 0
-                ? store.state.cartTotal + (deliveryType == "Standard" ? 5 : 10)
+                ? store.state.cartTotal + (deliveryType === "Standard" ? 5 : 10)
                 : 0}
             </h4>
           </div>
+
+          {/* Botón de finalización del pedido */}
           <div className="final-checkout">
             <button
               className="flat-button checkout"
               onClick={() => {
-                if (phone.length > 0) {
+                if (
+                  address &&
+                  city &&
+                  country &&
+                  state &&
+                  phone.length > 0
+                ) {
                   checkOut();
                   toast.info("Your order is being processed");
-                  return;
+                } else {
+                  toast.error("Please complete all the fields");
                 }
-                toast.error("Please enter your phone number");
               }}
               disabled={store.state.cartQuantity > 0 ? false : true}
             >
