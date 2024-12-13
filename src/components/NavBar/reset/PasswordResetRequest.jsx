5764bdc61258a56040d8a34174fc6350c154036a
michaelRS2002 <148493262+michaelRS2002@users.noreply.github.com>
2024-12-13 12:44:07 -0500

    Update PasswordResetRequest.jsx

--- src/components/NavBar/reset/PasswordResetRequest.jsx
+++ src/components/NavBar/reset/PasswordResetRequest.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { toast } from "react-toastify";
+import "./password_reset.css";
 
 const PasswordResetRequest = () => {
     const [email, setEmail] = useState("");
